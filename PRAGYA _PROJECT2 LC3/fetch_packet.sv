4c3aefd86b63c2a27baf090d95e8b2db3d72bc07
Pragya Gupta <43795554+pragyagupta-github@users.noreply.github.com>
2018-11-20 20:47:15 +0000

    Update fetch_packet.sv

--- PRAGYA _PROJECT2 LC3/fetch_packet.sv
+++ PRAGYA _PROJECT2 LC3/fetch_packet.sv
@@ -4,9 +4,9 @@ logic [15:0] taddr;
 logic [15:0] pc,npc_out;
 logic instrmem_rd;
 logic reset;
-/*constraint c1{taddr inside {[16'h3000:16'h5000]};
+constraint c1{taddr inside {[16'h3000:16'h5000]};
               br_taken inside {1'b1,1'b0};
               enable_updatepc inside {1'b1,1'b0};
-              enable_fetch inside {1'b1,1'b0};}*/
+              enable_fetch inside {1'b1,1'b0};}
              
 endclass
