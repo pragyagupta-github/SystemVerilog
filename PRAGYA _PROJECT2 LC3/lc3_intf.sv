87d678b8ba249d17cc732412d6687dd30556eb3b
Pragya Gupta <43795554+pragyagupta-github@users.noreply.github.com>
2018-11-20 21:00:39 +0000

    Update lc3_intf.sv

--- PRAGYA _PROJECT2 LC3/lc3_intf.sv
+++ PRAGYA _PROJECT2 LC3/lc3_intf.sv
@@ -9,5 +9,9 @@ modport drv(output complete_data, complete_instr,Instr_dout, Data_dout );
 modport mon(input pc, Data_addr, Data_din, instrmem_rd, Data_rd, I_macc, D_macc);
 
 assert property (@(posedge clock) (Instr_dout)|-> (pc));
+  
+  clocking cb @(posedge clk);
+    default input #1 output #0;
+  endclocking
 
 endinterface
